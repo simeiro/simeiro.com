edcc4d587cfa595ca3088667e368fec702d0c69e
simeiro <shitimi1633@gmail.com>
2024-11-20 20:29:53 +0900

    実装：ブログの最低限の機能

---
+++ simeiro/src/app/articles/page.tsx
@@ -0,0 +1,39 @@
+import Link from 'next/link';
+import { client } from '@/app/component/microcms';
+
+// ブログ記事の型定義
+type Props = {
+  id: string;
+  title: string;
+};
+
+// microCMSからブログ記事を取得
+async function getBlogPosts(): Promise<Props[]> {
+  const data = await client.get({
+    endpoint: 'articles', // 'blog'はmicroCMSのエンドポイント名
+    queries: {
+      fields: 'id,title',  // idとtitleを取得
+      limit: 5,  // 最新の5件を取得
+    },
+  });
+  return data.contents;
+}
+
+export default async function Home() {
+  const posts = await getBlogPosts();
+
+  return (
+    <main>
+      <h1>ブログ記事一覧</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>
+            <Link href={`/articles/${post.id}`}> {/* 記事へのリンクを生成 */}
+              {post.title} {/* タイトルを表示 */}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
+}
